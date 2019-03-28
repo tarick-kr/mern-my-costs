3994b502d2771976b7dead0a0d9a321f846d27f2
Sergey <tsg-kr@ukr.net>
2019-03-28 20:35:23 +0200

    kkkkk

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,6 @@ import { Container } from  'reactstrap';
 import { Provider } from 'react-redux';
 import store from './store';
 
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
